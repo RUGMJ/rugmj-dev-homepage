c22b2b8074d36ca1af9481e22e123740dca171b1
RUGMJ7443 <matthewrhysjones@icloud.com>
2022-04-11 18:40:25 +0100

    added, fullscreen for the info blocks, added auto contrasting colours for info blocks

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,44 @@ const main = document.querySelector('main');
 for (let i = 0; i < main.children.length; i++) {
 	const child = main.children[i];
 	const colour = randomColor({
-		luminosity: 'dark',
+		luminosity: 'random',
 		hue: 'random',
 	});
-	console.log(colour);
+	child.style.color = contrast(colour);
+	console.log(contrast(colour));
 	child.style.backgroundColor = colour;
+}
+
+function rgbToYIQ({ r, g, b }) {
+	return (r * 299 + g * 587 + b * 114) / 1000;
+}
+
+function hexToRgb(hex) {
+	if (!hex || hex === undefined || hex === '') {
+		return undefined;
+	}
+
+	const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+	return result
+		? {
+				r: parseInt(result[1], 16),
+				g: parseInt(result[2], 16),
+				b: parseInt(result[3], 16),
+		  }
+		: undefined;
+}
+
+function contrast(colorHex, threshold = 128) {
+	if (colorHex === undefined) {
+		return '#000';
+	}
+
+	const rgb = hexToRgb(colorHex);
+
+	if (rgb === undefined) {
+		return '#000';
+	}
+
+	return rgbToYIQ(rgb) >= threshold ? '#000' : '#fff';
 }
